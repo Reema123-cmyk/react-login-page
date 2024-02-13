6935fab982bded23a4e72ff078f4b0aa1c595fa6
Reema123-cmyk <159223021+Reema123-cmyk@users.noreply.github.com>
2024-02-13 23:01:45 +1100

    Add files via upload
    added public, react-project, src and few other files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ReactLoginPage from './Components/ReactLoginPage/ReactLoginPage';
+
+function App() {
+  return (
+    <div>
+      <ReactLoginPage/>  
+    </div>
+  );
+}
+
+export default App;
